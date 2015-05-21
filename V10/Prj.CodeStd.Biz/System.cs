c345bf7629102512ad4519cce9d6020ae7e3338d
d2nbe <dungnguyendinh.backend@hotmail.com>
2015-05-21 13:57:22 +0700

    zzz

---
+++ V10/Prj.CodeStd.Biz/System.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prj.CodeStd.Biz
+{
+    public class System
+    {
+        string strName = "D2NBE";
+        string strPhone = "0989550410";
+    }
+}
