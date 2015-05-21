cf98ede5623f0e6e211eb06a087b7e14d2b3f79c
d2nbe <dungnguyendinh.backend@hotmail.com>
2015-05-21 14:10:19 +0700

    zzz

---
+++ V10/Prj.CodeStd.Biz/VS13SBS/PII/Circle.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Prj.CodeStd.Biz.VS13SBS.PII
+{
+	class Circle
+	{
+		int radius;
+
+		double Area()
+		{
+			return Math.PI * radius * radius;
+		}
+	}
+}
